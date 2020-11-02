783e4de4405a18fa15dd2289dd453be05329d4e0
Sally Oh <59776016+min27604@users.noreply.github.com>
2020-11-02 16:07:09 +0900

    Gugudan

--- src/Day1/Gugu.java
+++ src/Day1/Gugu.java
@@ -14,8 +14,7 @@ public class Gugu {
         // Gugudan using for loop
         for (int i = start; i <= end; i++) {
             for (int j = 1; j <= 9; j++) {
-                int k = i * j;
-                System.out.println(i + " * " + j + " = " + k);
+                System.out.println(i + " * " + j + " = " + (i * j));
             }
         }
     }
