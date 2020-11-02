8abdc54acb2cbe17df2d9c5db5f0d8190a5ccec9
Sally Oh <59776016+min27604@users.noreply.github.com>
2020-11-02 16:03:07 +0900

    Gugudan

---
+++ src/Day1/Gugu.java
@@ -0,0 +1,22 @@
+package Day1;
+
+import java.util.Scanner;
+
+public class Gugu {
+    public static void main(String[] args) {
+
+        // Taking two inputs
+        System.out.println("시작단과 끝단을 입력해주세요. ");
+        Scanner input = new Scanner(System.in);
+        int start = input.nextInt();
+        int end = input.nextInt();
+
+        // Gugudan using for loop
+        for (int i = start; i <= end; i++) {
+            for (int j = 1; j <= 9; j++) {
+                int k = i * j;
+                System.out.println(i + " * " + j + " = " + k);
+            }
+        }
+    }
+}
