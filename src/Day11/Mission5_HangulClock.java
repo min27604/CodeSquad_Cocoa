2de5c84f20d9bd3bcd602d837e5496b56380070d
Sally Oh <min27604@hotmail.com>
2020-11-16 22:35:27 +0900

    refactored

--- src/Day11/Mission5_HangulClock.java
+++ src/Day11/Mission5_HangulClock.java
@@ -2,89 +2,84 @@ package Day11;
 
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Scanner;
 
 class Time {
     int hour;
     int minute;
     String period;
-    String timeNow;
+    String[] timeNow  = LocalTime.now().format(DateTimeFormatter.ofPattern("HH mm")).split(" ");
 
-    public void setHourMinutePeriod() {
-        LocalTime time = LocalTime.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH mm");
-        this.timeNow = time.format(formatter);
-        Scanner scan = new Scanner(timeNow);
-        this.hour = scan.nextInt();
-        this.minute = scan.nextInt();
+    public Time() {
+        this.hour = Integer.parseInt(timeNow[0]);
+        this.minute = Integer.parseInt(timeNow[1]);
         if (this.hour > 12) {
             this.period = "PM";
             this.hour -= 12;
             return;
         }
-        this.period = "PM";
+        this.period = "AM";
     }
 }
 
 class Checker {
     boolean[] checker = new boolean[36];
 
-    public void isMidDayOrNight(int hour, int minute, String period) {
-        if (hour == 0 && minute == 0) { // 자정, 정오
-            checker[24] = true;
-            if (period.equals("AM")) {
-                checker[18] = true;
+    public void isMidDayOrNight(Time time) {
+        if (time.hour == 0 && time.minute == 0) { // 자정, 정오
+            this.checker[24] = true; // '정'에 true
+            if (time.period.equals("AM")) {
+                this.checker[18] = true;
             }
-            if (period.equals("PM")) {
-                checker[30] = true;
+            if (time.period.equals("PM")) {
+                this.checker[30] = true;
             }
         }
     }
 
-    public void checkHour(int hour) {
-        checker[17] = true; // '시'
-        switch (hour) {
+    public void checkHour(Time time) {
+        this.checker[17] = true; // '시'
+        switch (time.hour) {
             case 1 :
-                checker[0] = true;
+                this.checker[0] = true;
                 break;
             case 2 :
-                checker[1] = true;
+                this.checker[1] = true;
                 break;
             case 3 :
-                checker[2] = true;
+                this.checker[2] = true;
                 break;
             case 4 :
-                checker[3] = true;
+                this.checker[3] = true;
                 break;
             case 5 :
-                checker[4] = checker[5] = true;
+                this.checker[4] = this.checker[5] = true;
                 break;
             case 6 :
-                checker[6] = checker[7] = true;
+                this.checker[6] = this.checker[7] = true;
                 break;
             case 7 :
-                checker[8] = checker[9] = true;
+                this.checker[8] = this.checker[9] = true;
                 break;
             case 8 :
-                checker[10] = checker[11] = true;
+                this.checker[10] = this.checker[11] = true;
                 break;
             case 9 :
-                checker[12] = checker[13] = true;
+                this.checker[12] = this.checker[13] = true;
                 break;
             case 10 :
-                checker[14] = true;
+                this.checker[14] = true;
                 break;
             case 11:
-                checker[14] = checker[15] = true;
+                this.checker[14] = this.checker[15] = true;
                 break;
             case 12:
-                checker[14] = checker[16] = true;
+                this.checker[14] = this.checker[16] = true;
         } // 해당 시(number)
     }
 
-    public void checkMinute(int minute) {
-        checker[35] = true; // '분'
-        switch (minute % 10) {
+    public void checkMinute(Time time) {
+        this.checker[35] = true; // '분'
+        switch (time.minute % 10) {
             case 1:
                 checker[25] = true;
                 break;
@@ -114,43 +109,43 @@ class Checker {
                 break;
 
         } // 해당 분(number)
-        if (minute > 9) {
-            checker[23] = true;
-            switch (minute / 10) {
+        if (time.minute > 9) {
+            this.checker[23] = true;
+            switch (time.minute / 10) {
                 case 2:
-                    checker[19] = true;
+                    this.checker[19] = true;
                     break;
                 case 3:
-                    checker[20] = true;
+                    this.checker[20] = true;
                     break;
                 case 4:
-                    checker[21] = true;
+                    this.checker[21] = true;
                     break;
                 case 5:
-                    checker[22] = true;
+                    this.checker[22] = true;
                     break;
 
             }
         }
     }
+
+    public Checker(Time time) {
+        if (time.hour == 0 && time.minute == 0) {
+            isMidDayOrNight(time);
+            return;
+        }
+        checkHour(time);
+        checkMinute(time);
+    }
 }
 
-public class Mission5_HangulClock {
-
+class HangulClock {
     public static final String ANSI_RESET = "\u001B[0m";
     public static final String ANSI_RED = "\u001B[31m";
+    char[] hangul = {'한', '두', '세', '네', '다', '섯', '여', '섯', '일', '곱', '여', '덟', '아', '홉', '열', '한', '두', '시',
+            '자', '이', '삼', '사', '오', '십', '정', '일', '이', '삼', '사', '육', '오', '오', '칠', '팔', '구', '분'};
 
-    public static void main(String[] args) {
-
-        Time time = new Time();
-        Checker checker = new Checker();
-        time.setHourMinutePeriod();
-        checker.isMidDayOrNight(time.hour, time.minute, time.period);
-        checker.checkHour(time.hour);
-        checker.checkMinute(time.minute);
-        char[] hangul = {'한', '두', '세', '네', '다', '섯', '여', '섯', '일', '곱', '여', '덟', '아', '홉', '열', '한', '두', '시',
-                '자', '이', '삼', '사', '오', '십', '정', '일', '이', '삼', '사', '육', '오', '오', '칠', '팔', '구', '분'};
-
+    public void displayHangulClock(Time time, Checker checker) {
         for (int i = 0; i < 36; i++) {
             if (i % 6 == 0) { System.out.println(); } // new lines after six letters
             if (checker.checker[i]) { // print in red if the letter's checked according to the time
@@ -159,5 +154,16 @@ public class Mission5_HangulClock {
             }
             System.out.print(hangul[i] + " "); // print letters
         }
+    }
+}
+
+public class Mission5_HangulClock {
+
+    public static void main(String[] args) {
+        Time time = new Time();
+        Checker checker = new Checker(time);
+        HangulClock hangulClock = new HangulClock();
+        hangulClock.displayHangulClock(time, checker);
+
     }
 }
